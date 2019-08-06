42d855280bfe1b35f2f33d86dfece568c612cb28
Ken89MathCompSci <lamerkenneth@hotmail.com>
2019-08-06 23:02:48 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+void rReverseAr(int ar[], int size);
+
+int main()
+{
+    int ar[10], size, i;
+
+    printf("Enter array size: ");
+    scanf("%d", &size);
+
+    printf("Enter %d numbers: ", size);
+
+    for(i=0; i<size; i++)
+        scanf("%d", &ar[i]);
+
+    rReverseAr(ar, size);
+
+    printf("rReverseAr(): ");
+
+    for (i=0; i<size; i++)
+        printf("%d ", ar[i]);
+
+    return 0;
+}
+
+void rReverseAr(int ar[], int size)
+{
+    int temp;
+
+    if (size == 0 || size == 1)
+        return;
+    else{
+        // swapping op
+        temp = ar[0];
+        ar[0] = ar[size-1];
+        ar[size-1] = temp;
+        rReverseAr(&ar[1], size-2);
+    }
+}
